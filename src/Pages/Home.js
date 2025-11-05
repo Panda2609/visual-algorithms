9c5be6483e672c4df4525ad02632ae8dca68955e
Panda2609 <banda2609@gmail.com>
2025-11-05 18:48:06 -0300

    FilterBar on Homepage"

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
+import FilterBar from "../components/filterBar/filterbar";
+
 function Home(){
     return(
         <>
         <div>
-            <h1>Hola al componente de home</h1>
-            <p>Esta aplicación será creada para visualizar algoritmos.</p>
+            <FilterBar />
         </div>
         </>
     );
