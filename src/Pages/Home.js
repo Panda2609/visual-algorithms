d8a872b9309bff82f9732967193c50e4e176acec
Panda2609 <banda2609@gmail.com>
2025-11-10 16:34:56 -0300

    add actionbar on home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
 import FilterBar from "../components/filterBar/FilterBar";
-
+import ActionBar from "../components/actionBar/ActionBar";
 function Home(){
     return(
         <>
         <div>
             <FilterBar />
+            <ActionBar />
         </div>
         </>
     );
