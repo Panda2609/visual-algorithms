8bee1d8505a7da444b6a87c84936fcd201728823
Panda2609 <banda2609@gmail.com>
2025-11-10 16:33:37 -0300

    Action bar component created

---
+++ src/components/actionBar/ActionBar.js
@@ -0,0 +1,12 @@
+
+import './ActionBar.css';
+
+function ActionBar() {
+    return (
+        <div className="action-bar">
+            <h1>action bar component!</h1>
+        </div>
+    );
+}
+
+export default ActionBar;
